c3f444dd5e452042ec970db4f678de1290f471bf
ScotDex <themadlyscientific@gmail.com>
2026-02-01 01:37:56 +0000

    tweet is trash

--- src/network/twitterService.js
+++ src/network/twitterService.js
@@ -13,13 +13,7 @@ const twitterClient = new TwitterApi({
 class TwitterService {
     static async postWhale (names, formattedValue, killId){
         try {
-            const status = `💥BOOM! A ${names.shipName} was destroyed!
-            
-            Value: ${formattedValue} ISK 
-            
-            https://zkillboard.com/kill/${killId}/ 
-            
-            #TweetFleet #EveOnline #SocketKill`;
+            const status = `💥BOOM! ${names.shipName} destroyed! || ${formattedValue} ISK || https://zkillboard.com/kill/${killId}/ || #TweetFleet #EveOnline #SocketKill || https://killstream.voidspark.org/`;
             await twitterClient.v2.tweet(status);
             console.log(`Tweet posted for Kill #${killId}`);
         } catch (err) {
