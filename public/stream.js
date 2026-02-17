efded468f49d6ca943ae7252b440080b89e9468c
ScottishDex <35346954+ScotDex@users.noreply.github.com>
2026-02-17 12:00:30 +0000

    refactoring

--- public/stream.js
+++ public/stream.js
@@ -194,7 +194,7 @@ socket.on('player-count', (data) => {
     const display = document.getElementById('player-count');
     const toast = document.getElementById('player-toast');
     if (data && data.active) {
-        toast.classList.remove('d-none');
+        toast.classList.remove('hidden');
         display.innerText = data.count.toLocaleString();
     } else {
         display.innerText = "OFFLINE";
