f74c6cbe5b0a265b08709936bfce154782195f2e
ScotDex <themadlyscientific@gmail.com>
2026-04-18 23:14:38 +0100

    value update

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -15,16 +15,21 @@ class utils {
 
   static formatIsk(rawValue) {
     const value = rawValue || 0;
-    if (value >= 1000) {
-      return `${(value / 1000).toFixed(2)}K`;
+
+    if (value >= 1_000_000_000_000) {
+      return `${(value / 1_000_000_000_000).toFixed(2)}T`;
     }
-    if (value >= 1000000000) {
-      return `${(value / 1000000000).toFixed(2)}B`;
+    if (value >= 1_000_000_000) {
+      return `${(value / 1_000_000_000).toFixed(2)}B`;
     }
-    if (value >= 1000000000000) {
-      return `${(value / 1000000000000).toFixed(2)}T`;
+    if (value >= 1_000_000) {
+      return `${(value / 1_000_000).toFixed(2)}M`;
     }
-    return `${(value / 1000000).toFixed(2)}M`;
+    if (value >= 1_000) {
+      return `${(value / 1_000).toFixed(2)}K`;
+    }
+
+    return value.toString();
   }
 
   static getZkillLink(killId) {
